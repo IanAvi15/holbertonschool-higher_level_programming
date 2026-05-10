a3148e4397b75353d0bd0bc72c09892f98e979db
IanAvi15 <icaviles20@hotmail.com>
2026-05-09 20:32:31 -0400

    checker being unbearable

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[36:66] + str[82:88] + str[:6])
+str = str[36:66] + str[82:88] + str[:6]
+print(str)
