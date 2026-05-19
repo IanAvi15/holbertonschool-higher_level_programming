5bf4a000e4c4b1cc18b7beff4c43c333ab4052d7
IanAvi15 <icaviles20@hotmail.com>
2026-05-19 17:11:39 -0400

    fixing syntax issue

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    """Convert a Roman numeral to an integer."""
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
@@ -9,12 +10,17 @@ def roman_to_int(roman_string):
     }
 
     total = 0
-    for i in range(len(roman_string)):
+    length = len(roman_string)
+
+    for i in range(length):
         value = roman_vals.get(roman_string[i], 0)
 
-        if i + 1 < len(roman_string) and value < roman_vals.get(roman_string[i + 1], 0):
-            total -= value
-        else:
-            total += value
+        if i + 1 < length:
+            next_val = roman_vals.get(roman_string[i + 1], 0)
+            if value < next_val:
+                total -= value
+                continue
+
+        total += value
 
     return total
