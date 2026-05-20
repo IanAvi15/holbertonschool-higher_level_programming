d7e4f58fbc18ec31ac25d1e1dd8cbd441316fcbb
IanAvi15 <icaviles20@hotmail.com>
2026-05-20 16:52:56 -0400

    fixing syntax

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -23,5 +23,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if isinstance(b, bool) or not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
