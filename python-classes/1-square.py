2af59f9c4bb9b2974687f92fb01b5b71894d5d86
IanAvi15 <icaviles20@hotmail.com>
2026-05-24 14:12:00 -0400

    adding task 1 exercise

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with a private size attribute."""
+
+
+class Square:
+    """A class that defines a square by its size.
+
+    Private Attributes:
+        __size: The size of the square (integer).
+    """
+
+    def __init__(self, size):
+        """Initialize a Square instance.
+
+        Args:
+            size: The size of the square (no type/value verification).
+        """
+        self.__size = size
