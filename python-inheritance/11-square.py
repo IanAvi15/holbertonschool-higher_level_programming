32f024e18139958fb9ba809b848d042c5e0c7590
IanAvi15 <icaviles20@hotmail.com>
2026-05-30 23:45:39 -0400

    adding task 11 class

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that provides a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square class with equal width and height and custom string output."""
+
+    def __init__(self, size):
+        """Initialize a Square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
