72e25257d9270d9504bac2adeb2397d1f281a78b
IanAvi15 <icaviles20@hotmail.com>
2026-06-07 23:03:25 -0400

    adding task 5 function

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that provides a function to save an object to a file as JSON."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using its JSON representation.
+
+    Args:
+        my_obj: The object to serialize and save.
+        filename (str): Path to the file to write.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
