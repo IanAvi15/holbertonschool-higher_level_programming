12fc12100ef8766718a5090ce133c1c006f1ca5e
IanAvi15 <icaviles20@hotmail.com>
2026-06-07 23:13:56 -0400

    fixing syntax error

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that defines a Student class with serialization and reload support."""
+"""Module that defines a Student class with serialization support."""
 
 
 class Student:
