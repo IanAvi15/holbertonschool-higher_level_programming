c32a6e5ae2a4cbf6a8d44ccf1a319f410f6f70d2
Ian Aviles <icaviles20@hotmail.com>
2026-07-05 19:07:47 -0400

    adding task 9 script

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- List cities.id, cities.name, and states.name, sorted by cities.id
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
