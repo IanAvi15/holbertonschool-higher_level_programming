e41ac6ed3ee40578a50dd85a45efce9cf571b3f7
IanAvi15 <icaviles20@hotmail.com>
2026-07-25 22:00:57 -0400

    saving change

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
-#!/usr/bin/node
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+#!/mingw64/bin/node
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
